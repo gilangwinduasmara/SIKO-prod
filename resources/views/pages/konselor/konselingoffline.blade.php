1d8827763596473ec14065c36b060eb428301886
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2021-05-20 19:40:01 +0700

    export buttons

--- resources/views/pages/konselor/konselingoffline.blade.php
+++ resources/views/pages/konselor/konselingoffline.blade.php
@@ -252,6 +252,14 @@
 
 @section("scripts")
     <script src="/plugins/custom/datatables/datatables.bundle.js" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
     <script>
         $('.date_range').datepicker({
             rtl: KTUtil.isRTL(),
@@ -305,6 +313,11 @@
             table = $('table').DataTable({
                 processing: true,
                 serverSide: true,
+                dom: 'Bfrtip',
+                buttons: [
+                    // 'copy', 'csv', 'excel', 'pdf', 'print'
+                    'excel', 'pdf', 'print'
+                ],
                 ajax: {
                     url: '/services/konselingoffline/dt',
                     type: 'GET',
@@ -342,6 +355,10 @@
                     } },
                 ]
             });
+            $('.dt-button').addClass('btn btn-outline-primary btn-shadow');
+            $('.buttons-excel').prepend('<i class="far fa-file-excel"></i>')
+            $('.buttons-pdf').prepend('<i class="far fa-file-pdf"></i>')
+            $('.buttons-print').prepend('<i class="fas fa-print"></i>')
             $('.dataTables_filter').hide();
             $('.date_range').change(function(){
                 var min  = $('#datepicker_dari').val();
